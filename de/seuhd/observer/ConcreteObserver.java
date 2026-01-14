6975dc5ad8b03c9d99218e44b523c3e8440a006a
Adam Balog <cu337@uni-heidelberg.de>
2026-01-14 14:33:49 +0100

    aufgabe 10.2.b) fertig

--- de/seuhd/observer/ConcreteObserver.java
+++ de/seuhd/observer/ConcreteObserver.java
@@ -1,18 +1,28 @@
 package de.seuhd.observer;
 
 public class ConcreteObserver implements Observer {
-    // TODO: Add fields and static variables.
+
+    private static int counter = 0;
+
+    private final int id;
+    private final Subject subject;
 
     public ConcreteObserver(Subject subject) {
-        // TODO: Implement constructor.
+        this.subject = subject;
+        this.id = ++counter;
+        subject.attach(this);
     }
 
     @Override
     public void update(Subject subject) {
-        // TODO: Implement update method.
+        if (subject instanceof ConcreteSubject concreteSubject) {
+            System.out.println(
+                    "Observer " + id + " received update from subject: New value is " + concreteSubject.getValue()
+            );
+        }
     }
 
     public void detach() {
-        // TODO: Implement detach method.
+        subject.detach(this);
     }
 }
